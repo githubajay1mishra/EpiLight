5d458b49d9d1fdf0768833f5c236945b1455bcd9
githubajay1mishra <ajay1.mishra@gmail.com>
2019-06-01 13:26:01 -0400

    Added LCA Soltion

---
+++ EpiLight/Chapter10BinaryTree/10.1/LCA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter10BinaryTree._10._1
+{
+    public class LeastCommonAncestor
+    {
+         public Node Find(Node root, int v1, int v2)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root.Data == v1 || root.Data == v2)
+            {
+                return root;
+            }
+
+            Node lcaInLeft   = Find(root.Left, v1, v2);
+            Node lcaInRight = Find(root.Right, v1, v2);
+
+            if (lcaInLeft != null && lcaInRight != null)
+            {
+                return root;
+            }
+
+            return lcaInLeft ?? lcaInRight;
+        }
+    }
+}
---
+++ EpiLight/Chapter10BinaryTree/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace Chapter10BinaryTree._10._1
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
